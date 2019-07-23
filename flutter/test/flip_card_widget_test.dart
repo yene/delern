f870ed3f30a9811efc4551376285c9acb2620ee6
Katarina Sheremet <kate.romanowskaya@gmail.com>
2019-07-23 13:03:05 +0200

    Test FlipCardWidget

---
+++ flutter/test/flip_card_widget_test.dart
@@ -0,0 +1,34 @@
+import 'package:delern_flutter/flutter/localization.dart';
+import 'package:delern_flutter/flutter/styles.dart' as app_styles;
+import 'package:delern_flutter/views/helpers/card_background_specifier.dart';
+import 'package:delern_flutter/views/helpers/flip_card_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Flip card', (tester) async {
+    const frontSide = 'der Vater';
+    const backSide = 'father';
+
+    // Widget must be wrapped in MaterialApp widget because it uses material
+    // related classes.
+    await tester.pumpWidget(MaterialApp(
+      localizationsDelegates: [
+        AppLocalizationsDelegate(),
+      ],
+      home: FlipCardWidget(
+        front: frontSide,
+        back: backSide,
+        backgroundColor: app_styles.cardBackgroundColors[Gender.masculine],
+        isMarkdown: false,
+      ),
+    ));
+    await tester.pumpAndSettle();
+    final frontFinder = find.text(frontSide);
+    expect(frontFinder, findsOneWidget);
+    await tester.tap(find.byType(Card));
+    await tester.pumpAndSettle();
+    final backFinder = find.text(backSide);
+    expect(backFinder, findsOneWidget);
+  });
+}

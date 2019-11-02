65da9ba3ba4db153dff1d89e844df6ea9761bebe
Katarina Sheremet <kate.romanowskaya@gmail.com>
2019-11-02 15:23:58 +0100

    Update link to tos

--- flutter/lib/flutter/legal.dart
+++ flutter/lib/flutter/legal.dart
@@ -1,2 +1,2 @@
 const String kPrivacyPolicy = 'https://www.iubenda.com/privacy-policy/69239405';
-const String kTermsOfService = 'https://delern.org/tos.html';
+const String kTermsOfService = 'https://dasfoo.org/delern-tos';

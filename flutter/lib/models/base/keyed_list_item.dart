1127747f652d3486958a982dff8b72b96273096e
Artem Sheremet <dot.doom@gmail.com>
2019-12-14 16:25:42 +0100

    Define indexOfKey on Iterable, not List
    This adds indexOfKey to BuiltList. Both BuitList and List implement
    Iterable, but BuiltList does not implement List.

--- flutter/lib/models/base/keyed_list_item.dart
+++ flutter/lib/models/base/keyed_list_item.dart
@@ -2,8 +2,20 @@ abstract class KeyedListItem {
   String get key;
 }
 
-extension KeyedListExtension<E extends KeyedListItem> on List<E> {
+extension KeyedIterableExtension<E extends KeyedListItem> on Iterable<E> {
   /// Returns an index of the first element where key equals to supplied [key].
   /// If such element is not found, returns -1.
-  int indexOfKey(String key) => indexWhere((item) => item.key == key);
+  int indexOfKey(String key) {
+    var index = -1;
+    // Can't use indexWhere because it's only defined on List, not Iterable:
+    // https://github.com/dart-lang/sdk/issues/30275.
+    firstWhere((item) {
+      index += 1;
+      return item.key == key;
+    }, orElse: () {
+      index = -1;
+      return null;
+    });
+    return index;
+  }
 }

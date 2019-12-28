23ec5f62b095013a758dc2f185962485ae7ebcd5
Katarina Sheremet <kate.romanowskaya@gmail.com>
2019-12-28 22:06:39 +0100

    Add Sentry

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,9 +1,7 @@
 import 'dart:async';
-import 'dart:isolate';
 
 import 'package:delern_flutter/flutter/localization.dart';
 import 'package:delern_flutter/flutter/styles.dart' as app_styles;
-import 'package:delern_flutter/remote/error_reporting.dart' as error_reporting;
 import 'package:delern_flutter/views/card_create_update/card_create_update.dart';
 import 'package:delern_flutter/views/decks_list/decks_list.dart';
 import 'package:delern_flutter/views/edit_deck/edit_deck.dart';
@@ -11,10 +9,10 @@ import 'package:delern_flutter/views/helpers/auth_widget.dart';
 import 'package:delern_flutter/views/onboarding/onboarding.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_sentry/flutter_sentry.dart';
 import 'package:pedantic/pedantic.dart';
 
 class App extends StatelessWidget {
@@ -58,38 +56,11 @@ class App extends StatelessWidget {
   }
 }
 
-Future<void> main() async {
-  unawaited(runZoned<Future>(() async {
-    // This is necessary to initialize Flutter method channels so that
-    // Crashlytics can call into the native code. It also must be in the same
-    // zone as the app: https://github.com/flutter/flutter/issues/42682.
-    WidgetsFlutterBinding.ensureInitialized();
-
-    // We report under a different project in dev mode.
-    // TODO(dotdoom): https://github.com/FirebaseExtended/flutterfire/pull/105.
-    // Crashlytics.instance.enableInDevMode = true;
-
-    FlutterError.onError = Crashlytics.instance.recordFlutterError;
-
-    final debugPrintWithoutCrashlytics = debugPrint;
-    debugPrint = (message, {wrapWidth}) {
-      Crashlytics.instance.log(message);
-      debugPrintWithoutCrashlytics(message, wrapWidth: wrapWidth);
-    };
-
-    Isolate.current.addErrorListener(RawReceivePort((pair) async {
-      final List<dynamic> errorAndStacktrace = pair;
-      await error_reporting.report(
-        'Isolate ErrorListener',
-        errorAndStacktrace.first,
-        errorAndStacktrace.last == null
-            ? null
-            : StackTrace.fromString(errorAndStacktrace.last),
-      );
-    }).sendPort);
-
-    unawaited(FirebaseDatabase.instance.setPersistenceEnabled(true));
-    unawaited(FirebaseAnalytics().logAppOpen());
-    runApp(App());
-  }, onError: Crashlytics.instance.recordError));
-}
+Future<void> main() => FlutterSentry.wrap(
+      () async {
+        unawaited(FirebaseDatabase.instance.setPersistenceEnabled(true));
+        unawaited(FirebaseAnalytics().logAppOpen());
+        runApp(App());
+      },
+      dsn: 'https://e6b5021448e14a49803b2c734621deae@sentry.io/1867466',
+    );

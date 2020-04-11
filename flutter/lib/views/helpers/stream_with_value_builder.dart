2d6ee74ab9dde7990d42d8eab4ee6441aae0a9e2
Artem Sheremet <dot.doom@gmail.com>
2020-04-11 09:12:26 +0200

    Make StreamBuilderWithValue a widget

--- flutter/lib/views/helpers/stream_with_value_builder.dart
+++ flutter/lib/views/helpers/stream_with_value_builder.dart
@@ -1,18 +1,42 @@
 import 'package:delern_flutter/models/base/stream_with_value.dart';
 import 'package:flutter/widgets.dart';
 
-/// Build a StreamBuilder widget with values supplied from StreamWithValue
-/// object.
-// We are not making this a separate Widget because of unnecessary overhead
-// associated with creating a class, defining all fields etc.
-StreamBuilder<T> buildStreamBuilderWithValue<T>({
+/// Build a [StreamBuilder] widget with values supplied from [StreamWithValue]
+/// object. DEPRECATED: use [StreamBuilderWithValue] widget instead.
+Widget buildStreamBuilderWithValue<T>({
   @required StreamWithValue<T> streamWithValue,
   @required AsyncWidgetBuilder<T> builder,
   Key key,
 }) =>
-    StreamBuilder<T>(
-      key: key,
-      initialData: streamWithValue.value,
-      stream: streamWithValue.updates,
+    StreamBuilderWithValue<T>(
+      streamWithValue: streamWithValue,
       builder: builder,
+      key: key,
     );
+
+@immutable
+class StreamBuilderWithValue<T> extends StatefulWidget {
+  final StreamWithValue<T> streamWithValue;
+  final AsyncWidgetBuilder<T> builder;
+  final Key _streamBuilderKey;
+
+  const StreamBuilderWithValue({
+    @required this.streamWithValue,
+    @required this.builder,
+    Key key,
+  }) : _streamBuilderKey = key;
+
+  @override
+  _StreamBuilderWithValueState<T> createState() =>
+      _StreamBuilderWithValueState<T>();
+}
+
+class _StreamBuilderWithValueState<T> extends State<StreamBuilderWithValue<T>> {
+  @override
+  Widget build(BuildContext context) => StreamBuilder<T>(
+        key: widget._streamBuilderKey,
+        initialData: widget.streamWithValue.value,
+        stream: widget.streamWithValue.updates,
+        builder: widget.builder,
+      );
+}

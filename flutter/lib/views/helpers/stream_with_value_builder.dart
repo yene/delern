a3e36d8bf696b467b28d0f702ca75fb8789a92a0
Artem Sheremet <dot.doom@gmail.com>
2020-04-25 19:17:20 +0200

    DataStreamWithValueBuilder without snapshots

--- flutter/lib/views/helpers/stream_with_value_builder.dart
+++ flutter/lib/views/helpers/stream_with_value_builder.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
+import 'package:delern_flutter/flutter/user_messages.dart';
 import 'package:delern_flutter/models/base/stream_with_value.dart';
-import 'package:flutter/widgets.dart';
+import 'package:delern_flutter/views/helpers/progress_indicator_widget.dart';
+import 'package:flutter/material.dart';
 
 /// Build a [StreamBuilder] widget with values supplied from [StreamWithValue]
 /// object. DEPRECATED: use [StreamBuilderWithValue] widget instead.
@@ -37,4 +41,78 @@ class _StreamBuilderWithValueState<T> extends State<StreamBuilderWithValue<T>> {
         stream: widget.streamWithValue.updates,
         builder: widget.builder,
       );
+}
+
+typedef DataTrigger<T> = void Function(T newValue);
+typedef DataBuilder<T> = Widget Function(BuildContext context, T value);
+
+class DataStreamWithValueBuilder<T> extends StatefulWidget {
+  final StreamWithValue<T> streamWithValue;
+  final DataBuilder<T> builder;
+  final DataTrigger<T> onData;
+
+  DataStreamWithValueBuilder({
+    @required this.streamWithValue,
+    @required this.builder,
+    this.onData,
+  }) : super(key: ValueKey(streamWithValue.updates));
+
+  @override
+  _DataStreamWithValueBuilderState<T> createState() =>
+      _DataStreamWithValueBuilderState<T>();
+}
+
+class _DataStreamWithValueBuilderState<T>
+    extends State<DataStreamWithValueBuilder<T>> {
+  StreamSubscription<T> _streamSubscription;
+  T _currentValue;
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (widget.streamWithValue.loaded) {
+      _currentValue = widget.streamWithValue.value;
+      if (widget.onData != null) {
+        scheduleMicrotask(() => widget.onData(_currentValue));
+      }
+    }
+
+    _streamSubscription = widget.streamWithValue.updates.listen((event) {
+      if (mounted) {
+        if (widget.onData != null) {
+          widget.onData(event);
+        }
+        setState(() {
+          _currentValue = event;
+        });
+      }
+    }, onDone: () {
+      if (mounted) {
+        Navigator.of(context).pop();
+      }
+    }, onError: (
+      dynamic e,
+      // https://github.com/dart-lang/linter/issues/1099
+      // ignore: avoid_types_on_closure_parameters
+      StackTrace stackTrace,
+    ) {
+      UserMessages.showAndReportError(
+        () => Scaffold.of(context),
+        e,
+        stackTrace: stackTrace,
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _streamSubscription.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => _currentValue == null
+      ? const ProgressIndicatorWidget()
+      : widget.builder(context, _currentValue);
 }
